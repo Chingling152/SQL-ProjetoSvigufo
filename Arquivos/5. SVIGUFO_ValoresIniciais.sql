47eb49c3566b271c10e9a49fdbe40aea98626c54
Franklin <Chingling152@hotmail.com>
2019-04-28 00:33:16 -0300

    Banco de dados melhorado

--- Arquivos/5. SVIGUFO_ValoresIniciais.sql
+++ Arquivos/5. SVIGUFO_ValoresIniciais.sql
@@ -1,4 +1,4 @@
-INSERT INTO TIPO_EVENTO VALUES('Design'),('Programação'),('Games');--valores pre iniciados
+INSERT INTO TIPOS_EVENTOS VALUES('Design'),('Programação'),('Games');--valores pre iniciados
 
-INSERT INTO INSTITUICAO 
+INSERT INTO INSTITUICOES 
 VALUES('Clarice Eletrônica ME','Soluções Eletrônicas Comércio e Representação','84559818000139','Rua Professor José Caetano dos Santos Mascarenhas, 232','03945030','SP','São Paulo');
